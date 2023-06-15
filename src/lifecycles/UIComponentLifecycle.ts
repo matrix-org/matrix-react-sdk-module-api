c645bb1d867c08f440db05c1b6220db42bdc3771
Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>
2023-06-15 17:59:52 +0300

    UIComponent enum updated
    Signed-off-by: Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>

--- src/lifecycles/UIComponentLifecycle.ts
+++ src/lifecycles/UIComponentLifecycle.ts
@@ -75,4 +75,14 @@ export enum UIComponent {
      * and integrations to the room, such as from the room information card.
      */
     AddIntegrations = "UIComponent.addIntegrations",
+
+    /**
+     * Component that lead to the user being able to search, dial, explore rooms
+     */
+    FilterContainer = "UIComponent.filterContainer",
+
+    /**
+     * Components that lead the user to room options menu.
+     */
+    RoomOptionsMenu = "UIComponent.roomOptionsMenu",
 }
