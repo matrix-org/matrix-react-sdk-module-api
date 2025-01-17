ed4966f04cf337064dc469c9e783116c4f3c5f74
Half-Shot <will@half-shot.uk>
2025-01-17 12:06:41 +0000

    Add favicon function extension.

--- src/lifecycles/BrandingExtensions.ts
+++ src/lifecycles/BrandingExtensions.ts
@@ -11,14 +11,42 @@ export interface AppTitleInRoom extends AppTitleBase {
     notificationsMuted: boolean;
 }
 
+export interface GetFaviconParameters {
+    // colour parameters
+    bgColor: string;
+    textColor: string;
+    // font styling parameters
+    fontFamily: string;
+    fontWeight: "normal" | "italic" | "bold" | "bolder" | "lighter" | number;
+
+    // positioning parameters
+    isUp: boolean;
+    isLeft: boolean;
+}
+
 export type AppTitleContext = AppTitleBase | AppTitleInRoom;
 
 export interface ProvideBrandingExtensions {
     getAppTitle(context: AppTitleContext): string | null;
+    /**
+     * Called when the app needs to generate the basic app favicon.
+     * @returns A string URL for the icon, or null if no new icon should be generated.
+     */
+    getFaviconSrc(): PromiseLike<string | null>;
+    /**
+     * Called when the app needs to generate a new "badge" favicon. 
+     * @param content The content inside the "badge".
+     * @param opts Extra parameters for the badge.
+     * @returns A string URL for the icon, or null if no new icon should be generated.
+     */
+    getFaviconSrc(content: number | string, opts: GetFaviconParameters): PromiseLike<string | null>;
 }
 
 export abstract class BrandingExtensionsBase implements ProvideBrandingExtensions {
-    public getAppTitle(context: AppTitleContext): string | null {
+    public getAppTitle(context: AppTitleContext) {
+        return null;
+    }
+    public async getFaviconSrc(content?: number | string, opts?: GetFaviconParameters) {
         return null;
     }
 }
