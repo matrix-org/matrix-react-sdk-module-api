a88b9eae6f82200468a5a7eee7dff45390f51d1b
Half-Shot <will@half-shot.uk>
2025-01-17 12:11:26 +0000

    Update docs and types.

--- src/lifecycles/BrandingExtensions.ts
+++ src/lifecycles/BrandingExtensions.ts
@@ -1,19 +1,42 @@
 interface AppTitleBase {
+    /**
+     * The configured brand name.
+     * @example Element
+     */
     brand: string;
+    /**
+     * Is the client sync loop in an error state.
+     */
     syncError: boolean;
+    /**
+     * The current unread notification count.
+     */
+    unreadNotificationCount?: number;
+    /**
+     * Has the client muted notifications.
+     */
+    notificationsMuted?: boolean;
 }
 
 export interface AppTitleInRoom extends AppTitleBase {
-    brand: string;
+    /**
+     * The room name, may be undefined if the room has no name.
+     */
     roomName?: string;
+    /**
+     * The room ID of the room in view.
+     */
     roomId: string;
-    unreadNotificationCount: number;
-    notificationsMuted: boolean;
 }
 
 export type AppTitleContext = AppTitleBase | AppTitleInRoom;
 
 export interface ProvideBrandingExtensions {
+    /**
+     * Called whenever the client would update the document title.
+     * @param context Current application context used to generate the title.
+     * @returns A string to be used for the full title, or null to use the default title.
+     */
     getAppTitle(context: AppTitleContext): string | null;
 }
 
